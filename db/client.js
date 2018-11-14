9222257cb55d3079f62a5a0a720592e5c1e7c336
John Burge <john@burge.io>
2018-11-13 19:00:35 -0600

    added db-start script and more data handlers

--- db/client.js
+++ db/client.js
@@ -1,3 +1,8 @@
+/*
+  Currently there's nothing to the client logic; however, putting the client in its own script
+  allows us to extend the client logic in the future (or change the client completely).
+*/
+
 const mongoAdapter = require('mongodb');
 
 module.exports = mongoAdapter.MongoClient;
--- db/config/index.js
+++ db/config/index.js
@@ -2,12 +2,17 @@ const dbName = require('./config.json').instance.dbName;
 const usersSchema = require('../schemas/users.json')
 
 const dbConfig = (dbConnection) => {
+  console.log('DB CONNECTION:', dbConnection);
   return dbConnection.then(conn => {
+    console.log('CONN:', conn);
     // Configure 'users' collection to have an enforced schema
     return conn.db(dbName).createCollection('users', { validator: { $jsonSchema: usersSchema } });
   })
-    .then(collection => collection.createIndex({ 'email': 1 }, { 'unique': true }))
-    .catch(err => { throw err; }) // bubble error up through promise chain
+    .then(collection => {
+      console.log('COLLECTION:', collection);
+      return collection.createIndex({ 'email': 1 }, { 'unique': true });
+    })
+    .catch(err => Promise.reject(err)) // bubble error up through promise chain
 }
 
 module.exports = dbConfig;
